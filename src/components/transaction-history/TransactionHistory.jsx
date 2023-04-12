65ee50a006eef65870d243aad245a63797cfc3ca
Alina1Alina <119854810+Alina1Alina@users.noreply.github.com>
2023-04-12 20:05:58 +0300

    httt

--- src/components/transaction-history/TransactionHistory.jsx
+++ src/components/transaction-history/TransactionHistory.jsx
@@ -25,4 +25,12 @@ export const TransactionHistory = ({ items }) => {
   );
 };
 
+TransactionHistory.propTypes = {
+    items: PropTypes.arrayOs(PropTypes.shape({
+        type: PropTypes.string.isRequired,
+        amount: PropTypes.string.isRequired,
+        currency: PropTypes.string.isRequired
+    }))
+}
+
 
